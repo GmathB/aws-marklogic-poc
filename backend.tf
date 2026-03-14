eb525d1a4b43ab75ab2d73199b29291e65ea500a
Gomathi <gomathirohini@gmail.com>
2026-03-14 17:53:08 +0530

    Fix backend configuration - remove unsupported use_lockfile parameter

--- backend.tf
+++ backend.tf
@@ -1,10 +1,9 @@
 
  terraform {
    backend "s3" {
-     bucket         = "marklogic-terraform-state-013596899729"  # Your AWS Account ID
+     bucket         = "marklogic-terraform-state-013596899729"
      key            = "marklogic/terraform.tfstate"
      region         = "ap-south-1"
      encrypt        = true
-     use_lockfile   = true  # Modern S3-native state locking (replaces deprecated DynamoDB)
    }
  }
