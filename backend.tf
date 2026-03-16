500e5659e78e5d4b41d9dfe3188cdf8285736c95
Gomathi <gomathirohini@gmail.com>
2026-03-16 12:26:54 +0530

    Add s3:PutEncryptionConfiguration and s3:PutBucketObjectLockConfiguration permissions

--- backend.tf
+++ backend.tf
@@ -5,7 +5,9 @@
      key     = "marklogic/terraform.tfstate"
      region  = "ap-south-1"
      encrypt = true
-     # use_lockfile = true  # I get an error as `Not a valid S3 backend parameter - use DynamoDB for state locking`
+     # use_lockfile = true  
+     
+     # I get an error as `Not a valid S3 backend parameter - use DynamoDB for state locking`
      # But this is not true. dynamodb will be deprecated soon and I wanted to test native locking.
      # Will be investigating this further.
    }
