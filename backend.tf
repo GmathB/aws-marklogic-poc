88e5ed74c5f73ee8bcae7f35660cf7c2eda67b0f
Gomathi <gomathirohini@gmail.com>
2026-03-16 11:07:47 +0530

    Errors updated for s3 native lock

--- backend.tf
+++ backend.tf
@@ -1,10 +1,12 @@
 
  terraform {
    backend "s3" {
-     bucket       = "marklogic-terraform-state-013596899729"
-     key          = "marklogic/terraform.tfstate"
-     region       = "ap-south-1"
-     encrypt      = true
-     use_lockfile = true
+     bucket  = "marklogic-terraform-state-013596899729"
+     key     = "marklogic/terraform.tfstate"
+     region  = "ap-south-1"
+     encrypt = true
+     # use_lockfile = true  # I get an error as `Not a valid S3 backend parameter - use DynamoDB for state locking`
+     # But this is not true. dynamodb will be deprecated soon and I wanted to test native locking.
+     # Will be investigating this further.
    }
  }
